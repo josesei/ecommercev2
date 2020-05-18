7840d3fa8b33f0ab83ddd8b7604b428d84e69bb2
josesei <joseignaciocarbone@gmail.com>
2020-05-17 21:43:12 -0300

    added dependencies and started setting up nodejs configurations and routes

--- nodejs/src/app.ts
+++ nodejs/src/app.ts
@@ -1,7 +1,40 @@
-import {Product} from "./product";
+import {} from "./entities/product";
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
 
 const productionUrl = "";
-const developmentUrl = "http://localhost"
+const developmentUrl = "http://localhost";
+
+//DB CONNECTION
+mongoose.connect("mongodb://localhost:27017/ecommercev2", {useNewUrlParser: true}, (err:any)=>{
+    if(err) {
+        console.error("Could not connect to MongoDB");
+    }
+    else{
+        console.log("Connected to MongoDB")
+    }
+});
+
+
+
+//GENERAL FORMATTING
+app.use(bodyParser.urlencoded({
+    extended: true
+}),
+bodyParser.json());
+
+
+//ROUTES
+app.use(require("./routes/productRoute"));
+
+
+//SERVER CONFIG
+const port = process.env.PORT || 3000;
+app.listen(port, ()=>{
+    console.log(`Listening on port ${port}...`);
+})
 
 
 
---
+++ nodejs/src/routes/productRoute.ts
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const express = require("express");
+const productRouter = express.Router()
+
+
+productRouter.get("/product/:id", (req:any, res:any)=>{
+    res.end();
+});
+
+module.exports = productRouter;
